4ee982224ee7106b8cfc7975070f701dd4c9db93
Akshay <akshaynagaraj16112005@gmail.com>
2026-08-18 17:48:39 +0530

    binary search in 2D array: return true if element is present in matrix

--- searching/binary_search_in_2D_array/binary_search_in_2D_array.java
+++ searching/binary_search_in_2D_array/binary_search_in_2D_array.java
@@ -5,7 +5,7 @@ public class binary_search_in_2D_array {
     // basic binary search or 2D array
     static int[] search(int[][] matrix,int target){
         int r = 0;
-        int c = matrix.length - 1;
+        int c = matrix[0].length - 1;
 
         while(r < matrix.length && c >= 0){
             if(matrix[r][c] == target){
@@ -20,13 +20,27 @@ public class binary_search_in_2D_array {
         System.out.println("Not found element ");
         return new int[]{-1,-1};
     }
+    
+    // return true if element present in matrix
+    static boolean find(int[][] matrix, int target){
+        int r = 0;
+        int c = matrix[0].length - 1;
+
+        while(r < matrix.length && c >= 0){
+            if(matrix[r][c] == target) return true;
+            else if(matrix[r][c] < target) r++;
+            else c--;
+        }
+
+        return false;
+    }
+    
     public static void main(String[] args){
         int[][] matrix = {
             {10,20,30,40},
             {15,25,35,45}
         };
-        int[] result = search(matrix,55);
-        System.out.println(matrix[result[0]][result[1]]);
+        System.out.println(find(matrix,20));
     }
 
 }
