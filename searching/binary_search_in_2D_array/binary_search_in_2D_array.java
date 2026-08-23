c92fa24fdca4b317fe13e52462d7853c379a750b
Akshay <akshaynagaraj16112005@gmail.com>
2026-08-23 21:42:18 +0530

    solving problem on 2D array binary search

--- searching/binary_search_in_2D_array/binary_search_in_2D_array.java
+++ searching/binary_search_in_2D_array/binary_search_in_2D_array.java
@@ -21,15 +21,28 @@ public class binary_search_in_2D_array {
         return new int[]{-1,-1};
     }
     
-    // return true if element present in matrix
-    static boolean find(int[][] matrix, int target){
+    // return true if element present in matrix, column wise sorted
+    static boolean findc(int[][] matrix, int target){
         int r = 0;
         int c = matrix[0].length - 1;
-
         while(r < matrix.length && c >= 0){
             if(matrix[r][c] == target) return true;
             else if(matrix[r][c] < target) r++;
             else c--;
+        }
+
+        return false;
+    }
+
+    // return true if element present in matrix, row wise sorted 
+    static boolean findr(int[][] matrix,int target){
+        int r = matrix.length - 1;
+        int c = 0;
+
+        while(c < matrix[0].length && r >=0){
+            if(matrix[r][c] == target) return true;
+            else if(target > matrix[r][c]) c++;
+            else r--;
         }
 
         return false;
@@ -40,7 +53,7 @@ public class binary_search_in_2D_array {
             {10,20,30,40},
             {15,25,35,45}
         };
-        System.out.println(find(matrix,20));
+        System.out.println(findr(matrix,30));
     }
 
 }
